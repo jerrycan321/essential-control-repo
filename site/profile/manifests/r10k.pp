2e847218138e0d4492bc7b0c3463222412194606
jerrycan321 <jerrycan321@gmail.com>
2019-09-26 11:55:19 +0100

    Update r10k.pp

--- site/profile/manifests/r10k.pp
+++ site/profile/manifests/r10k.pp
@@ -1,6 +1,6 @@
 class profile::10k {
   class {'r10k':
-    remote => 'https://github.com/jerrycan321/essential-control-repo.git',
+    remote => 'https://github.com/jerrycan321/essential-control-repo',
   }
   class {'r10k::webhook::config':
     use_mcollective => false,
